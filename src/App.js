695992f93f0035b819c5a41d4a6b98cad3cc7d7b
sorphorn <so.sorphorn@gmail.com>
2019-03-17 19:31:30 +0700

    finish fix navbar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import React, {Component} from 'react';
-// import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {Navbar,Nav, FormControl, Button, Form} from 'react-bootstrap';
+// import {Navbar,Nav, FormControl, Button, Form} from 'reactstrap';
 import './App.css';
 import Routes from './Routes';
 
 class App extends Component{
   render() {
     return (
-      <div className="App container">
-        <Navbar fluid collapseOnSelect> 
-           {/* <Navbar.Brand>
-              <Link to="/">
-                Scratch
-              </Link>
-            </Navbar.Brand>
-            <Navbar.Toggle /> */}
-            <Navbar bg="dark" variant="dark">
-              <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-              <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-              </Nav>
-              <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-info">Search</Button>
-              </Form>
-            </Navbar>
-        </Navbar>  
-        <Routes /> 
-      </div>
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">Rotati</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Signup</Nav.Link>
+          <Nav.Link href="#features">Login</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-light">Search</Button>
+        </Form>
+      </Navbar>
     );
   } 
 }
